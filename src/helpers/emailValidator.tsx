0de4a8ba8734521b33e2c4f4d2fb1fe731a6144c
Jinnapat Yana <jaja_yana@hotmail.com>
2023-12-03 16:11:30 +0700

    add email validate helper function

---
+++ src/helpers/emailValidator.tsx
@@ -0,0 +1,8 @@
+const validateEmail = (email: string) => {
+  if (email === "") return false;
+  const regex =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  return regex.test(email);
+};
+
+export default validateEmail;
