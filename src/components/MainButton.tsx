031e1eb9ad61c94f348459007095b5a47d8d825a
Jinnapat Yana <jaja_yana@hotmail.com>
2023-12-03 16:09:28 +0700

    add common components

---
+++ src/components/MainButton.tsx
@@ -0,0 +1,33 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+
+const MainButton = ({
+  children,
+  handler,
+  enable,
+  loading,
+}: {
+  children: JSX.Element;
+  handler: () => void;
+  enable?: boolean;
+  loading: boolean;
+}) => {
+  return (
+    <button
+      className="rounded-full w-full border-green-400 border-2 p-2 bg-black transition-colors disabled:bg-black disabled:text-gray-400 hover:bg-green-800 duration-300"
+      onClick={handler}
+      disabled={!enable}
+    >
+      {loading ? (
+        <div className="flex flex-row gap-2 justify-center items-center">
+          <FontAwesomeIcon icon={faSpinner} className="animate-spin" />
+          processing...
+        </div>
+      ) : (
+        children
+      )}
+    </button>
+  );
+};
+
+export default MainButton;
---
+++ src/components/ThreeDotsLoading.tsx
@@ -0,0 +1,11 @@
+const ThreeDotsLoading = () => {
+  return (
+    <div className="flex flex-row gap-1 py-1">
+      <div className="bg-gray-700 w-2 h-2 rounded-full animate-bounce"></div>
+      <div className="bg-gray-700 w-2 h-2 rounded-full animate-bounce"></div>
+      <div className="bg-gray-700 w-2 h-2 rounded-full animate-bounce"></div>
+    </div>
+  );
+};
+
+export default ThreeDotsLoading;
