3a8fd32d59621283a379fae51668f50f82e956a2
Jinnapat Yana <genesoft.ai@gmail.com>
2024-01-12 02:40:39 +0700

    add supabase client

---
+++ src/supabase/client.tsx
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+
+const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
+const SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_KEY as string;
+const supabaseClient = createClient(SUPABASE_URL, SUPABASE_KEY);
+
+export default supabaseClient;
